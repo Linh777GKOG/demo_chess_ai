fb564bb353ff4ac570d3f6ab2d3192b6bc21f0f3
Linh Vu <linh777gkog@gmail.com>
2022-07-04 23:27:27 +0700

    render moves history

--- script.js
+++ script.js
@@ -215,3 +215,18 @@ var getBestMove = function (game) {
   $('#positions-per-s').text(positionsPerS);
   return bestMove;
 };
+
+var renderMoveHistory = function (moves) {
+  var historyElement = $('#move-history').empty();
+  historyElement.empty();
+  for (var i = 0; i < moves.length; i = i + 2) {
+    historyElement.append(
+      '<span>' +
+        moves[i] +
+        ' ' +
+        (moves[i + 1] ? moves[i + 1] : ' ') +
+        '</span><br>'
+    );
+  }
+  historyElement.scrollTop(historyElement[0].scrollHeight);
+};
