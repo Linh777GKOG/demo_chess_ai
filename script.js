56a1cdbb562f51fbd413148ee4b31806b54c53ae
Linh Vu <linh777gkog@gmail.com>
2022-07-04 23:26:46 +0700

    return value

--- script.js
+++ script.js
@@ -149,3 +149,28 @@ var kingEvalWhite = [
 ];
 
 var kingEvalBlack = reverseArray(kingEvalWhite);
+
+var getPieceValue = function (piece, x, y) {
+  if (piece === null) {
+    return 0;
+  }
+  var getAbsoluteValue = function (piece, isWhite, x, y) {
+    if (piece.type === 'p') {
+      return 10 + (isWhite ? pawnEvalWhite[y][x] : pawnEvalBlack[y][x]);
+    } else if (piece.type === 'r') {
+      return 50 + (isWhite ? rookEvalWhite[y][x] : rookEvalBlack[y][x]);
+    } else if (piece.type === 'n') {
+      return 30 + knightEval[y][x];
+    } else if (piece.type === 'b') {
+      return 30 + (isWhite ? bishopEvalWhite[y][x] : bishopEvalBlack[y][x]);
+    } else if (piece.type === 'q') {
+      return 90 + evalQueen[y][x];
+    } else if (piece.type === 'k') {
+      return 900 + (isWhite ? kingEvalWhite[y][x] : kingEvalBlack[y][x]);
+    }
+    throw 'Unknown piece type: ' + piece.type;
+  };
+
+  var absoluteValue = getAbsoluteValue(piece, piece.color === 'w', x, y);
+  return piece.color === 'w' ? absoluteValue : -absoluteValue;
+};
